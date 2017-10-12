511e36662f79aedbcc2b1f173708c231b0b3adc7
Andrey Matrosov <matrosovs2008@yandex.ru>
2017-10-13 00:00:28 +0300

    init commit

---
+++ src/main/java/by/matrosov/springsecmysql/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package by.matrosov.springsecmysql.service;
+
+import by.matrosov.springsecmysql.model.CustomUserDetails;
+import by.matrosov.springsecmysql.model.User;
+import by.matrosov.springsecmysql.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
+        Optional<User> optionalUser = userRepository.findByName(s);
+
+        optionalUser.orElseThrow(()->new UsernameNotFoundException("Name not found"));
+
+        return optionalUser.map(CustomUserDetails::new).get();
+    }
+}
