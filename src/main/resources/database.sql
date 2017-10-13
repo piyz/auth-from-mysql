690a4f46fbfa0f871928a707542e3b5d63ebcd38
Andrey Matrosov <matrosovs2008@yandex.ru>
2017-10-13 12:23:54 +0300

    add db

---
+++ src/main/resources/database.sql
@@ -0,0 +1,25 @@
+
+
+CREATE TABLE user(
+  user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  active INT NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE role(
+  role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  role_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE user_role(
+  user_id INT NOT NULL,
+  role_id INT NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES user(user_id),
+  FOREIGN KEY (role_id) REFERENCES role(role_id),
+
+  UNIQUE (user_id,role_id)
+);
